97da4fd7c9df5c9014f2ec663ef4f7d146fc0d69
kienhoangcaotroc <108188816+kienhoangcaotroc@users.noreply.github.com>
2022-09-17 15:01:18 +0700

    TITANEDU
    first-commit

---
+++ script.js
@@ -0,0 +1,126 @@
+
+/*HEADER*/
+window.addEventListener("scroll", myFunction);
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+var sticky = 170;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+/*CAROUSEL SLIDE*/
+var cl = document.querySelector(".carousel__slide");
+var slides = document.querySelectorAll(".slide")
+var prev = document.querySelector(".prev");
+var next = document.querySelector(".next");
+
+var counter = 1;
+var width = slides[0].clientWidth;
+cl.style.transform = "translateX("+ (counter* (-width)) + "px)";
+console.log(width)
+
+next.addEventListener("click", function() {
+   cl.style.transition = "transform 0.4s ease-in-out";
+    counter = counter + 1;
+   cl.style.transform = "translateX("+ (counter* (-width)) + "px)";
+})
+
+prev.addEventListener("click", function() {
+   cl.style.transition = "transform 0.4s ease-in-out";
+    counter = counter - 1;
+   cl.style.transform = "translateX("+ (counter* (-width)) + "px)";
+})
+
+cl.addEventListener("transitionend", function() {
+   if (slides[counter].id === "first") {
+   cl.style.transition = "none";
+    counter = slides.length -counter;
+   cl.style.transform = "translateX("+ (counter* (-width)) + "px)";
+}
+   if (slides[counter].id === "last") {
+   cl.style.transition = "none";
+    counter = slides.length - 2;
+   cl.style.transform = "translateX("+ (counter* (-width)) + "px)";
+}
+})
+
+
+
+
+/*FEEDBACK*/
+
+var cl2 = document.querySelector(".fb__slide");
+var sl = document.querySelectorAll(".slide2")
+
+var counterr = 1;
+var width = sl[0].clientWidth;
+cl2.style.transform = "translateX("+ (counterr* (-width)) + "px)";
+console.log(width)
+
+setInterval(function() {
+   counterr++;
+   autoTransition(counterr)
+},3000)
+
+function autoTransition(counterr) {
+   cl2.style.transition = "transform 0.5s ease-in-out";
+   cl2.style.transform = "translateX("+ (counterr* (-width)) + "px)";
+}
+
+cl2.addEventListener("transitionend", function() {
+   console.log(sl)
+   if (sl[counterr].id === "first1") {
+   cl2.style.transition = "none";
+     counterr = sl.length -counterr;
+   cl2.style.transform = "translateX("+ (counterr* (-width)) + "px)";
+}
+   if (sl[counterr].id === "last1") {
+   cl2.style.transition = "none";
+     counterr = sl.length - 2;
+   cl2.style.transform = "translateX("+ (counterr* (-width)) + "px)";
+}
+})
+
+
+/* HELP ANIMATION */
+
+
+
+
+
+var text = document.getElementById("main-title");
+var line = text.offsetTop;
+var line = 160;
+
+function animatedText() {
+  if (window.pageYOffset > line) {
+    text.classList.add("line");
+  } else {
+    text.classList.remove("line");
+  }
+}
+window.addEventListener("scroll", animatedText);
+/* function animatedText() {
+   var text = document.getElementById("main-title");
+ 
+   for (var i = 0; i < text.length; i++) {
+     var windowHeight = window.innerHeight;
+     var elementTop = text[i].getBoundingClientRect().top;
+     var elementVisible = 170;
+ 
+     if (elementTop < windowHeight - elementVisible) {
+       text[i].classList.add("line");
+     } else {
+       text[i].classList.remove("line");
+     }
+   }
+ }
+ 
+ window.addEventListener("scroll", animatedText); */
